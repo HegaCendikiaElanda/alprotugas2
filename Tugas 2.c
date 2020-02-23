cddc108ca584be56a5f55ed953d107e45a3ad3f1
Hega Cendikia Elanda <42016968+HegaCendikiaElanda@users.noreply.github.com>
2020-02-23 17:01:32 +0700

    Rata-rata sudah

--- Tugas 2.c
+++ Tugas 2.c
@@ -4,6 +4,8 @@
 
 FILE *newFile;
 
+int totalHarga;
+
 typedef struct{
 	char kode[6];
 	char nama[25];
@@ -18,6 +20,20 @@ void tampilMenu(){
 	printf("3. Hitung Total Produk\n");
 	printf("4. Hitung rata-rata total produk\n");
 	printf("5. exit\n");
+}
+
+int cekFile(){
+	int status,size;
+	newFile = fopen("Arsip.txt","r");
+	fseek(newFile, 0, SEEK_END);
+	size = ftell(newFile);
+	if (size == 0){
+		status = 0;
+	}else{
+		status = 1;
+	}
+	fclose(newFile);
+	return status;
 }
 
 void buatArsip(){
@@ -36,37 +52,51 @@ void buatArsip(){
 }
 
 void tampilArsip(){
-	newFile=fopen("Arsip.txt","r");
-	printf("Isi Arsip Produk\n");
-	fscanf(newFile, "%s %s %d\n",&produk.kode, &produk.nama, &produk.harga );
-	if(strcmp(produk.kode, "XXX") == 0){
-		printf("arsip kosong\n");
+	if(cekFile()==1){
+		newFile=fopen("Arsip.txt","r");
+		printf("Isi Arsip Produk\n");
+		fscanf(newFile, "%s %s %d\n",&produk.kode, &produk.nama, &produk.harga );
+		if(strcmp(produk.kode, "XXX") == 0){
+			printf("Arsip Kosong\n");
+		}else{
+			do{
+				printf("Kode : %s\n",produk.kode);
+				printf("Nama Produk  : %s\n",produk.nama);
+				printf("Harga : %d\n",produk.harga);
+				printf("-----------------------\n");
+				fscanf(newFile, "%s %s %d\n", &produk.kode, &produk.nama, &produk.harga);
+			}while(strcmp(produk.kode, "XXX") != 0);
+		}   
+		fclose(newFile);
 	}else{
-		do{
-			printf("Kode : %s\n",produk.kode);
-			printf("Nama Produk  : %s\n",produk.nama);
-			printf("Harga : %d\n",produk.harga);
-			printf("-----------------------\n");
-			fscanf(newFile, "%s %s %d\n", &produk.kode, &produk.nama, &produk.harga);
-		}while(strcmp(produk.kode, "XXX") != 0);
-	}   
-	fclose(newFile);
+		printf("Arsip Kosong\n");
+	}
 }
 
 int hitungTotalProduk(){
-	int totalHarga = 0;
-	// randy
-	return totalHarga;
+	// Randy
 }
 
-int hitungRataProduk(){
-	int rataProduk = 0;
-	//nenden
-	return rataProduk;
+float hitungRataProduk(){
+	float total,hasil;
+	int jmlhproduk=0;
+	if(cekFile()==1){
+		newFile=fopen("Arsip.txt","r");
+		fscanf(newFile, "%s %s %d\n",&produk.kode, &produk.nama, &produk.harga );
+		do{
+			total=total+produk.harga;	
+			jmlhproduk++;
+			fscanf(newFile, "%s %s %d\n",&produk.kode, &produk.nama, &produk.harga );
+		}while(strcmp(produk.kode, "XXX") != 0);
+		hasil=total/jmlhproduk;
+		fclose(newFile);
+	}
+	return hasil;
 }
 
 main(){
-	int pilih,tp,rt;
+	int pilih,tp;
+	float rt;
 	do{
 		system("cls");
 		tampilMenu();
@@ -76,11 +106,11 @@ main(){
 			case 2: tampilArsip(); break;
 			case 3 : 
 				tp = hitungTotalProduk();
-				printf("Total produk : %d"),tp;
+				printf("Total produk : %d",tp);
 				break;
 			case 4 :
 				rt = hitungRataProduk();
-				printf("Rata-rata produk : %d",rt);
+				printf("Rata-rata produk : %f",rt);
 				break;
 			case 5: printf("Aplikasi keluar....."); break;
 			default : printf("Input tidak valid !!\n");
